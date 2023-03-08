0a9a4354a28a02e7703716f527ed61a902017c3f
manohar icon <manohar icon@LAPTOP-KFOVULAQ>
2023-03-08 13:08:11 +0530

    Property file Created

--- src/test/java/utilities/PropertyFileUtil.java
+++ src/test/java/utilities/PropertyFileUtil.java
@@ -1,5 +1,15 @@
 package utilities;
 
+import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropertyFileUtil {
+	 public static String getValueForKey(String key) throws Throwable {
+		 Properties config = new Properties();
+		 config.load(new FileInputStream("C:\\OJTLiveProject\\ERP_StockAccountingHybridFrame\\PropertyFile\\Environment.Properties"));
+		return config.getProperty(key);
+	 }
 
 }
