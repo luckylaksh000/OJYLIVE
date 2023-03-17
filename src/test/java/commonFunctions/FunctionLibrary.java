4565376578f052a0f7fe54d2ee9f291bef37d85f
manohar icon <manohar icon@LAPTOP-KFOVULAQ>
2023-03-17 19:07:28 +0530

    ExcelMethods

--- src/test/java/commonFunctions/FunctionLibrary.java
+++ src/test/java/commonFunctions/FunctionLibrary.java
@@ -1,5 +1,37 @@
 package commonFunctions;
 
-public class FunctionLibrary {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 
+import utilities.PropertyFileUtil;
+/*Project Name:Stock Accounting
+ *  Tester:Narendra
+ * Created Date:11/3/2023
+ * Module Name:All Modules
+ * 
+ */
+
+public class FunctionLibrary {
+	public static WebDriver driver;
+//Method for Start Browser
+	public static WebDriver startBrowser()throws Throwable
+	{
+		if(PropertyFileUtil.getValueForKey("Browser").equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver","C:\\Selenium Data\\chromedriver_win32 (4)\\chromedriver.exe");
+			WebDriver driver= new ChromeDriver();
+			driver.manage().window().maximize();
+			
+		}
+		else if(PropertyFileUtil.getValueForKey("Browser").equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.firefox.driver"," C:\\Selenium Data\\chromedriver_win32 (4)\\chromedriver.exe");
+			WebDriver driver = new FirefoxDriver();
+		}
+		else
+		{
+			Reporter.log("Browser value is Not Matching");
+		}
+		return driver;
+	}
 }
--- src/test/java/utilities/ExcelFileUtil.java
+++ src/test/java/utilities/ExcelFileUtil.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
+import org.apache.poi.ss.formula.functions.Index;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
@@ -15,92 +16,92 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class ExcelFileUtil {
 	Workbook wb;
-	//construction for reading ExcelPath
+	//Constructor for Reading Excel Path
 	public ExcelFileUtil(String ExcelPath) throws Throwable {
 		FileInputStream file = new FileInputStream(ExcelPath);
-		wb= WorkbookFactory.create(file);
-		
+		wb=WorkbookFactory.create(file);
+
+
 	}
-	//method for counting rows
-	public int rowCount(String sheetName) {
-		return wb.getSheet(sheetName).getLastRowNum();
-		
-	}
-	//Read cell data
-	public String getcellData(String sheetName,int row, int column )
+	//Method for Counting Rows
+	public int rowCount(String sheetName)
 	{
-		String data="";
-		if(wb.getSheet(sheetName).getRow(row).getCell(column).getCellType()==Cell.CELL_TYPE_NUMERIC) {
-			int celldata =(int) wb.getSheet(sheetName).getRow(row).getCell(column).getNumericCellValue();
-			data=String.valueOf(celldata);
+		return wb.getSheet(sheetName).getLastRowNum();
+
+	}
+	//read Cell Data
+	public String getcelldata(String sheetName, int row, int column)
+	{
+		String data= "";
+		if(wb.getSheet(sheetName).getRow(row).getCell(column).getCellType()==Cell.CELL_TYPE_NUMERIC)
+		{
+			int celldata= (int) wb.getSheet(sheetName).getRow(row).getCell(column).getNumericCellValue();
+			data= String.valueOf(celldata);
+
 		}
-		else {
-			data = wb.getSheet(sheetName).getRow(row).getCell(column).getStringCellValue();
+		else
+		{
+			data= wb.getSheet(sheetName).getRow(row).getCell(column).getStringCellValue();
 		}
 		return data;
 	}
-		//method for set cell data(writing status in new workbook)pass,fail,blocked
-		public void setcellData(String sheetName, int row, int column, String status, String writeExcel ) throws Throwable {
-			//get sheet from wb
-			Sheet ws = wb.getSheet(sheetName);
-			//get row from sheet
-			Row rowNum= ws.getRow(row);
-			//create cell
-			Cell cell = rowNum.createCell(column);
-			//write status
-			cell.setCellValue(status);
-			if(status.equalsIgnoreCase("pass")) {
-				CellStyle style = wb.createCellStyle();
-				Font font = wb.createFont();
-				font.setColor(IndexedColors.GREEN.getIndex());
-				font.setBold(true);
-				font.setBoldweight(font.BOLDWEIGHT_BOLD);
-				style.setFont(font);
-				rowNum.getCell(column).setCellStyle(style);
-				
-				
-				
-			}
-				
-			
-			
+
+
+//method for Setcell data(fir writing)
+public void setCellData(String sheetName, int row, int column, String status, String writeExcel) throws Throwable
+{
+	//get sheet from workbook
+	Sheet ws =wb.getSheet(sheetName);
+	//get row from sheet
+	Row rowNum = ws.getRow(row);
+	//create cell
+	Cell cell = rowNum.createCell(column);
+	//write status
+	cell.setCellValue(status);
+	if (status.equalsIgnoreCase("pass"))
 		
+	{
+		CellStyle style = wb.createCellStyle();
+		Font font = wb.createFont();
+		font.setColor(IndexedColors.GREEN.getIndex());
+		font.setBold(true);
+		font.setBoldweight(font.BOLDWEIGHT_BOLD);
+		style.setFont(font);
+		rowNum.getCell(column).setCellStyle(style);
 		
-		else if(status.equalsIgnoreCase("Fail")) {
-			CellStyle style = wb.createCellStyle();
-			Font font = wb.createFont();
-			font.setColor(IndexedColors.RED.getIndex());
-			font.setBold(true);
-			font.setBoldweight(font.BOLDWEIGHT_BOLD);
-			style.setFont(font);
-			rowNum.getCell(column).setCellStyle(style);
-			
+	}
+	else if(status.equalsIgnoreCase("Fail"))
+	{
+		CellStyle style = wb.createCellStyle();
+		Font font = wb.createFont();
+		font.setColor(IndexedColors.RED.getIndex());
+		font.setBold(true);
+		font.setBoldweight(font.BOLDWEIGHT_BOLD);
+		style.setFont(font);
+		rowNum.getCell(column).setCellStyle(style);
 	
-}
-		else if(status.equalsIgnoreCase("Blocked")) {
-				CellStyle style = wb.createCellStyle();
-				Font font = wb.createFont();
-				font.setColor(IndexedColors.BLUE.getIndex());
-				font.setBold(true);
-				font.setBoldweight(font.BOLDWEIGHT_BOLD);
-				style.setFont(font);
-				rowNum.getCell(column).setCellStyle(style);
-				
-
-
-}
-	FileOutputStream fo = new FileOutputStream(writeExcel);
-			wb.write(fo);
-
-
-		}
-
-
-
+		
+	}
+	else if(status.equalsIgnoreCase("Blocked"))
+	{
+		CellStyle style = wb.createCellStyle();
+		Font font = wb.createFont();
+		font.setColor(IndexedColors.BLUE.getIndex());
+		font.setBold(true);
+		font.setBoldweight(font.BOLDWEIGHT_BOLD);
+		style.setFont(font);
+		rowNum.getCell(column).setCellStyle(style);
+	
+		
+	}
+	FileOutputStream file = new FileOutputStream(writeExcel);
+	wb.write(file);
+			
 }
 
 
 
+}
 
 
 
